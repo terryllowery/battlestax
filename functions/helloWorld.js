4abe8f395a7d9a02d6a9223e71970769568673e8
Terry Lowery <tl@lowerytech.net>
2020-12-09 14:11:30 -0600

    bumping it

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,5 +1,6 @@
 exports.handler = async (event, context) => {
   // let's return a JSON response that looks like: { hello: "world" }
+  // bumping it
   return {
     statusCode: 200,
     body: JSON.stringify({ hello: "world"}),
